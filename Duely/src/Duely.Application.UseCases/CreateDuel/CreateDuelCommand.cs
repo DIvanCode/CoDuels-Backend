39195f76f28799f9dcc7cdf830964fb8a2b500d2
daria <d4riyabel@yandex.ru>
2025-10-04 00:01:01 +0300

    implement CreateDuelHandler, fix types

--- Duely/src/Duely.Application.UseCases/CreateDuel/CreateDuelCommand.cs
+++ Duely/src/Duely.Application.UseCases/CreateDuel/CreateDuelCommand.cs
@@ -5,6 +5,6 @@ namespace Duely.Application.UseCases.CreateDuel;
 
 public record CreateDuelCommand : IRequest<Result>
 {
-    public required string User1Id { get; init; }
-    public required string User2Id { get; init; }
+    public required int User1Id { get; init; }
+    public required int User2Id { get; init; }
 }
