66d6bf0525247368d0fa9dae490f659415900dfa
daria <d4riyabel@yandex.ru>
2025-10-13 00:46:41 +0300

    fix DuelStartedMessage

--- Duely/src/Duely.Infrastructure.Gateway.Client.Abstracts/Messages/DuelStartedMessage.cs
+++ Duely/src/Duely.Infrastructure.Gateway.Client.Abstracts/Messages/DuelStartedMessage.cs
@@ -4,11 +4,9 @@ namespace Duely.Infrastructure.Gateway.Client.Abstracts;
 
 public sealed class DuelStartedMessage : Message
 {
+    [JsonIgnore]
     public override MessageType Type => MessageType.DuelStarted;
 
     [JsonPropertyName("duel_id")]
     public int DuelId { get; init; }
-    public int User1Id { get; init; }
-    public int User2Id { get; init; }
-    public string TaskId { get; init; } = string.Empty;
 }
