e7f91c98193e19386118e3f530fde837268f23eb
Ivan Dobrynin <i.dobrynin@tbank.ru>
2025-10-17 13:16:11 +0300

    feat(Taski): Get random task

---
+++ Taski/internal/usecase/task/usecase/random/usecase.go
@@ -0,0 +1,29 @@
+package random
+
+import (
+	"fmt"
+	"log/slog"
+	"taski/internal/domain/task"
+)
+
+type (
+	Query struct {
+	}
+
+	UseCase struct {
+		log     *slog.Logger
+	}
+)
+
+func NewUseCase(log *slog.Logger) *UseCase {
+	return &UseCase{log: log}
+}
+
+func (uc *UseCase) Random(query Query) (taskID task.ID, err error) {
+	// temp: hardcode only one existing task id
+	if err = taskID.FromString("7d971f50363cf0aebbd87d971f50363cf0aebbd8"); err != nil {
+		err = fmt.Errorf("failed to parse task id: %w")
+		return
+	}
+	return
+}
