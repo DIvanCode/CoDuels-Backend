41f09565dfa37cf57b3fcda88fda1669d57091bc
dariabelll <151754936+dariabelll@users.noreply.github.com>
2025-11-02 20:18:46 +0300

    feat(Duely): Refactoring backgroundJobs (#78)

--- Duely/src/Duely.Application.BackgroundJobs/DuelMakingJob.cs
+++ Duely/src/Duely.Application.BackgroundJobs/DuelMakingJob.cs
@@ -1,5 +1,4 @@
-﻿using Duely.Domain.Services.Duels;
-using MediatR;
+﻿using MediatR;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,7 +6,7 @@ using Duely.Application.UseCases.Features.Duels;
 
 namespace Duely.Application.BackgroundJobs;
 
-public sealed class DuelMakingJob(IServiceProvider sp, IDuelManager duelManager, IOptions<DuelMakingJobOptions> options)
+public sealed class DuelMakingJob(IServiceProvider sp, IOptions<DuelMakingJobOptions> options)
     : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
@@ -17,18 +16,8 @@ public sealed class DuelMakingJob(IServiceProvider sp, IDuelManager duelManager,
             using (var scope = sp.CreateScope())
             {
                 var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-                var pair = duelManager.TryGetPair();
 
-                if (pair is not null)
-                {
-                    var command = new CreateDuelCommand
-                    {
-                        User1Id = pair.Value.User1,
-                        User2Id = pair.Value.User2
-                    };
-
-                    await mediator.Send(command, cancellationToken);
-                }
+                await mediator.Send(new TryCreateDuelCommand(), cancellationToken);
             }
 
             await Task.Delay(options.Value.CheckPairIntervalMs, cancellationToken);
