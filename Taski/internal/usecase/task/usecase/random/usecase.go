b71f6423a414ef6cc9010fbd053a3d36e32b8915
divancode <divancode@gmail.com>
2025-11-04 11:37:29 +0300

    c611ca79dbe5d03a3e103a45f85ca097167b0a7e

--- Taski/internal/usecase/task/usecase/random/usecase.go
+++ Taski/internal/usecase/task/usecase/random/usecase.go
@@ -21,7 +21,7 @@ func NewUseCase(log *slog.Logger) *UseCase {
 
 func (uc *UseCase) Random(query Query) (taskID task.ID, err error) {
 	// temp: hardcode only one existing task id
-	if err = taskID.FromString("0c5fe950363fc0aeb7d80c5fe950363fc0aeb7d8"); err != nil {
+	if err = taskID.FromString("c611ca79dbe5d03a3e103a45f85ca097167b0a7e"); err != nil {
 		err = fmt.Errorf("failed to parse task id: %w")
 		return
 	}
