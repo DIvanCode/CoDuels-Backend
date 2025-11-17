a416fe4376cdee64a1d19bcbd67fcdade1034ca0
Ivan Petrukhin <eewynell@gmail.com>
2025-11-17 22:24:26 +0300

    fix: fmt.Errorf format

--- Taski/internal/usecase/task/usecase/random/usecase.go
+++ Taski/internal/usecase/task/usecase/random/usecase.go
@@ -22,7 +22,7 @@ func NewUseCase(log *slog.Logger) *UseCase {
 func (uc *UseCase) Random(query Query) (taskID task.ID, err error) {
 	// temp: hardcode only one existing task id
 	if err = taskID.FromString("7d971f50363cf0aebbd87d971f50363cf0aebbd8"); err != nil {
-		err = fmt.Errorf("failed to parse task id: %w")
+		err = fmt.Errorf("failed to parse task id: %w", err)
 		return
 	}
 	return
