1cfc2c395129ad0882ba5123776fbba4992620e5
divancode <divancode@gmail.com>
2025-11-24 23:37:29 +0300

    migration

---
+++ Duely/src/Duely.Infrastructure.DataAccess.EntityFramework/Migrations/20251124203655_DuelRatingDelta.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Duely.Infrastructure.DataAccess.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class DuelRatingDelta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "User1RatingDelta",
+                table: "Duels",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "User2RatingDelta",
+                table: "Duels",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "User1RatingDelta",
+                table: "Duels");
+
+            migrationBuilder.DropColumn(
+                name: "User2RatingDelta",
+                table: "Duels");
+        }
+    }
+}
