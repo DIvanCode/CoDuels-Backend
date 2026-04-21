f7f062e28b908bff5209e87151442480cb6272f8
Иван Добрынин <49978381+DIvanCode@users.noreply.github.com>
2026-04-22 00:39:50 +0300

    fix(duely): code run migration bug (#231)

---
+++ Duely/src/Duely.Infrastructure.DataAccess.EntityFramework/Migrations/20260421213721_RunCodeJobCategoryAndLimits.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Duely.Infrastructure.DataAccess.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class RunCodeJobCategoryAndLimits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DuelId",
+                table: "CodeRuns",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaskKey",
+                table: "CodeRuns",
+                type: "varchar(1)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CodeRuns_DuelId_TaskKey",
+                table: "CodeRuns",
+                columns: new[] { "DuelId", "TaskKey" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CodeRuns_DuelId_TaskKey",
+                table: "CodeRuns");
+
+            migrationBuilder.DropColumn(
+                name: "DuelId",
+                table: "CodeRuns");
+
+            migrationBuilder.DropColumn(
+                name: "TaskKey",
+                table: "CodeRuns");
+        }
+    }
+}
